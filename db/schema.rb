bf837ddfbc958c6fb65df06d555809391b11d7d4
anthonygharvey <anthony@anthonygharvey.com>
2019-03-13 23:00:49 -0400

    Added due_date and user_id to Tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190314024905) do
+ActiveRecord::Schema.define(version: 20190314025957) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.integer  "priority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "due_date"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
