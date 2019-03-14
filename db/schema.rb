32a3a0772195c77f10170678900eb2a4aef2ac71
anthonygharvey <anthony@anthonygharvey.com>
2019-03-13 22:59:15 -0400

    Generated User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102162126) do
+ActiveRecord::Schema.define(version: 20190314024905) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.integer  "priority"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
